949023db36e19e290c82209bb825effc00ca7749
Nick Ulle <nick.ulle@gmail.com>
2016-12-12 16:05:27 -0800

    Update lemonbuddy to polybar.

--- bspwm/.config/polybar/launch.sh
+++ bspwm/.config/polybar/launch.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
 # Terminate already running bar instances.
-killall -q lemonbuddy
+killall -q polybar
 
 # Wait until the processes have been shut down.
-while pgrep -x lemonbuddy > /dev/null; do sleep 1; done
+while pgrep -x polybar > /dev/null; do sleep 1; done
 
 # Launch bar.
-lemonbuddy master &
+polybar master &
 
 echo "Bar launched..."
