930ab64621200914620ae4b4412ddc841880abd9
Nick Ulle <nick.ulle@gmail.com>
2023-02-27 21:37:11 -0800

    [Neovim] Set up some autocommands.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,7 @@
 require("base")
 require("highlights")
 require("maps")
+require("autocmd")
 
 local has = function(x)
   return vim.fn.has(x) == 1
---
+++ nvim/.config/nvim/lua/autocmd.lua
@@ -0,0 +1,57 @@
+
+local optl = vim.opt_local
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+
+-- Treat .Rprofile files as R files.
+autocmd({"BufNewFile", "BufRead"}, {
+  pattern = {".Rprofile", "rprofile"},
+  group = augroup("rprofile", {clear = true}),
+  callback = function()
+    optl.filetype = "r"
+  end
+})
+
+
+-- Treat BUGS files as R files.
+autocmd({"BufNewFile", "BufRead"}, {
+  pattern = "*.bug",
+  group = augroup("bugs", {clear = true}),
+  callback = function()
+    optl.filetype = "r"
+  end
+})
+
+
+-- Enable spell-checking for text formats.
+autocmd({"FileType"}, {
+  pattern = {"text", "markdown", "rmd"},
+  group = augroup("spell", {clear = true}),
+  callback = function()
+    optl.spell = true
+    optl.spelllang = "en_us"
+  end
+})
+
+
+-- Python
+autocmd({"FileType"}, {
+  pattern = "python",
+  group = augroup("python", {clear = true}),
+  callback = function()
+    -- Use 4 spaces per tab, as per PEP8.
+    optl.tabstop = 4
+  end
+})
+
+
+-- Git commit messages
+autocmd({"FileType"}, {
+  pattern = "gitcommit",
+  group = augroup("git", {clear = true}),
+  callback = function()
+    optl.textwidth = 72
+    optl.colorcolumn = "73"
+  end
+})
