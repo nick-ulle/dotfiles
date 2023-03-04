64ccc3aa0cb79d09a7cb98b986544503f511c874
Nick Ulle <nick.ulle@gmail.com>
2023-03-03 19:15:30 -0800

    [Neovim] Update plugins config.

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,25 +1,60 @@
 return {
-  -- Aesthetics
-  {
-    "RRethy/nvim-base16",
-    lazy = false,
-    priority = 100,
-    config = function()
-      vim.cmd("colorscheme base16-solarized-light")
-    end
-  },
+  -- Interface
   {
     "nvim-lualine/lualine.nvim",
+    name = "lualine",
     dependencies = {"kyazdani42/nvim-web-devicons"},
+    opts = {},
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    name = "gitsigns",
+    opts = {},
+  },
+  {
+    "NvChad/nvim-colorizer.lua",
+    name = "colorizer",
+    opts = {
+      filetypes = { "*", "!mason" },
+    },
+  },
+  {
+    "folke/which-key.nvim",
     config = function()
-      require("lualine").setup {}
+      vim.o.timeoutlen = 800
+      require("which-key").setup {
+        layout = {
+          height = { min = 2, max = 10 }
+        },
+        popup_mappings = {
+          scroll_down = "<down>",
+          scroll_up = "<up>",
+        }
+      }
     end
   },
 
-  -- Other plugins
-  "tpope/vim-surround",
-  "PeterRincker/vim-argumentative",
+  -- Behaviors
+  {
+    "numToStr/Comment.nvim",
+    name = "Comment",
+    lazy = false,
+    opts = {},
+    config = function(plugin, opts)
+      require(plugin.name).setup(opts)
+      vim.keymap.set("n", "<leader>c", "gcc", { remap = true })
+      vim.keymap.set("v", "<leader>c", "gc", { remap = true })
+    end
+  },
+  {
+    "kylechui/nvim-surround",
+    tag = "v1.0.0",
+    lazy = false,
+    opts = {},
+  },
+
   "ntpeters/vim-better-whitespace",
+  -- Vimscript plugins
   {
     "junegunn/vim-easy-align",
     config = function()
@@ -28,4 +63,3 @@ return {
   },
   --"mfussenegger/nvim-dap"
 }
-
